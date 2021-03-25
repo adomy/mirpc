f9c93bf70d9ed27445822ffb31dc9b79ad1767fb
寻喻 <adomy.zjl@antfin.com>
2021-03-25 09:43:56 +0800

    refactor(REGISTRY): mirpc-admin重命名为mirpc-registry
    新名称更合适，同时也是将功能剥离为独立的module，包括后续的监控、治理等功能

--- mirpc-registry/src/main/java/com/adomy/mirpc/registry/MirpcRegistryApplication.java
+++ mirpc-registry/src/main/java/com/adomy/mirpc/registry/MirpcRegistryApplication.java
@@ -1,13 +1,13 @@
-package com.adomy.mirpc.admin;
+package com.adomy.mirpc.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MirpcAdminApplication {
+public class MirpcRegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MirpcAdminApplication.class, args);
+        SpringApplication.run(MirpcRegistryApplication.class, args);
     }
 
 }
--- mirpc-registry/src/test/java/com/adomy/mirpc/registry/MirpcRegistryApplicationTests.java
+++ mirpc-registry/src/test/java/com/adomy/mirpc/registry/MirpcRegistryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.adomy.mirpc.admin;
+package com.adomy.mirpc.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MirpcAdminApplicationTests {
+class MirpcRegistryApplicationTests {
 
     @Test
     void contextLoads() {
